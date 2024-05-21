4b4dfef0341621bb179e108900cb878ff2eb52ad
Taichi H <taichi.h.acct@gmail.com>
2024-05-21 21:26:38 +0900

    時間割変更時の表示不具合を修正

---
+++ Noti2meWidget/AppIntent.swift
@@ -0,0 +1,18 @@
+//
+//  AppIntent.swift
+//  Noti2meWidget
+//
+//  Created by 細川泰智 on 2024/04/09.
+//
+
+import WidgetKit
+import AppIntents
+
+struct ConfigurationAppIntent: WidgetConfigurationIntent {
+    static var title: LocalizedStringResource = "Configuration"
+    static var description = IntentDescription("This is an example widget.")
+
+    // An example configurable parameter.
+    @Parameter(title: "Favorite Emoji", default: "😃")
+    var favoriteEmoji: String
+}
---
+++ Timetable with SwiftData/Views/SettingViews/QRCodeView.swift
@@ -0,0 +1,8 @@
+//
+//  QRCodeView.swift
+//  Timetable with SwiftData
+//
+//  Created by 細川泰智 on 2024/04/02.
+//
+
+import Foundation
